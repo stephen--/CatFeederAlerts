0eb3e67d16a222c28394efe714a77f80f94255bc
Stephen <stephen.common@gmail.com>
2017-10-14 18:44:23 -0700

    Update googleScript.js

--- googleScript.js
+++ googleScript.js
@@ -1,27 +1,31 @@
 function DetectMeals(){
-  var lastDate = getEpoch(getSpreadSheetValue());
-  var d = new Date();
-  var currentDate = getEpoch(Utilities.formatDate(d, 'America/Los_Angeles', "MMMM dd, yyyy 'at' hh:mm a").toString());
+  var lastDate = getEpoch(getSpreadSheetValue(),25200000);
+  var currentDate_input = Utilities.formatDate((new Date()), 'GMT-7', "MMMM dd, yyyy 'at' hh:mm a").toString();
+  var currentDate = getEpoch(currentDate_input);   
   var currentHour = epochToDate(currentDate)[0];
   var currentMinute = epochToDate(currentDate)[1];
   var currentDayPart = epochToDate(currentDate)[2];
-  var lastHour = epochToDate(lastDate)[0];
-  var lastMinute = epochToDate(lastDate)[1];
-  var lastDayPart = epochToDate(lastDate)[2];
-  
+  var currentTime = currentHour+':'+currentMinute+' '+currentDayPart;
+  var lastHour = epochToDate(lastDate,-25200000)[0];
+  var lastMinute = epochToDate(lastDate,-25200000)[1];
+  var lastDayPart = epochToDate(lastDate,-25200000)[2];
+  var lastTime = lastHour+':'+lastMinute+' '+lastDayPart;
+
+  var DebugTime = 'Last '+lastTime+' | Current: '+currentTime;
+
   var error = false;
   var errorState = '';
-  var debug = 'Outside of reporting hours.';
+  var debug = 'Outside of reporting hours. ('+DebugTime+')';
   if (currentHour == 4 && currentMinute >= 29 && currentMinute <= 59 && currentDayPart == 'AM') {
     debug += 'A1: Success | ';
     if (lastHour == 4 && lastDayPart == 'AM') {
       error = false;
       errorState = 'Succes: 4AM meal was dispensed';
-      debug += 'A1.A: Success| ';
+      debug += 'A1.A: Success ('+DebugTime+') | ';
     } else {
       error = true;
       errorState = '4AM meal was not dispensed';
-      debug += 'A1.B: Failed (Last Hour: '+lastHour+' AM/PM '+lastDayPart+') | ';
+      debug += 'A1.B: Failed (Last Hour: '+lastHour+' AM/PM '+lastDayPart+') ('+DebugTime+') | ';
     }
   }
   if (currentHour == 11 && currentMinute >= 29 && currentMinute <= 59 && currentDayPart == 'AM') {
@@ -29,11 +33,11 @@ function DetectMeals(){
     if (lastHour == 11 && lastDayPart == 'AM') {
       error = false;
       errorState = 'Succes: 11AM meal was dispensed';
-      debug += 'B1.A: Success| ';
+      debug += 'B1.A: Success ('+DebugTime+') | ';
     } else {
       error = true;
       errorState = '11AM meal was not dispensed';
-      debug += 'BB1.B: Failed (Last Hour: '+lastHour+' AM/PM '+lastDayPart+') | ';
+      debug += 'BB1.B: Failed (Last Hour: '+lastHour+' AM/PM '+lastDayPart+') ('+DebugTime+') | ';
     }
   }
   if (currentHour == 5 && currentMinute >= 29 && currentMinute <= 59 && currentDayPart == 'PM') {
@@ -41,27 +45,15 @@ function DetectMeals(){
     if (lastHour == 5 && lastDayPart == 'PM') {
       error = false;
       errorState = 'Succes: 5PM meal was dispensed';
-      debug += 'C1.A: Success| ';
+      debug += 'C1.A: Success ('+DebugTime+') | ';
     } else {
       error = true;
       errorState = '5PM meal was not dispensed';
-      debug += 'C1.B: Failed (Last Hour: '+lastHour+' AM/PM '+lastDayPart+') | ';
+      debug += 'C1.B: Failed (Last Hour: '+lastHour+' AM/PM '+lastDayPart+') ('+DebugTime+') | ';
     }
   }
 
   errorReporting(error,errorState,debug,currentDate);
-
-  Logger.log('     ================================');
-  Logger.log('     Error:            ' +error);
-  Logger.log('     ErrorState:    '+errorState);
-  Logger.log('     Debug:          ' +debug);
-  Logger.log('     ================================');
-  Logger.log('     currentDate: '+currentDate);
-  Logger.log('     lastDate:       '+lastDate);
-  Logger.log('     ================================');
-  Logger.log('     cD Array:      '+epochToDate(currentDate));
-  Logger.log('     lD Array:       '+epochToDate(lastDate));
-  Logger.log('     ================================');
 }
 
 function getSpreadSheetValue(){
@@ -70,6 +62,7 @@ function getSpreadSheetValue(){
   var filter = Avals.filter(String).length;
   var rowText = Avals[filter - 1][0];
   var lastDate = extractDate.exec(rowText)[1].toString();
+  var lastDate = lastDate.replace('PM',' PM').replace('AM',' AM');
   return lastDate;
 }
 
@@ -78,9 +71,9 @@ function errorReporting(error,errorState,debug,currentEpochTime){
   var myTextEmail = "if_your_carrier_supports_email_to_Text_put_it_here@urCarrier.com
 
   var d = new Date(currentEpochTime*1000);
-  var currentTime = Utilities.formatDate(d, 'America/Los_Angeles', "hh:mm a").toString();
-  var currentDate = Utilities.formatDate(d, 'America/Los_Angeles', "MMMM dd, yyyy").toString();
-
+  var currentTime = Utilities.formatDate(d, Session.getScriptTimeZone(), "hh:mm a").toString();
+  var currentDate = Utilities.formatDate(d, Session.getScriptTimeZone(), "MMMM dd, yyyy").toString();
+  
   // Initiate error reporting (Spreadsheet logging / Email Alerts)
   if (error){
     // Send e-mail
@@ -95,11 +88,7 @@ function errorReporting(error,errorState,debug,currentEpochTime){
       subject: "",
       htmlBody: errorState
     });
-    
     var emailQuotaRemaining = MailApp.getRemainingDailyQuota();
-    Logger.log("Remaining email quota: " + emailQuotaRemaining);
-    Logger.log('Appended error to "Errors" sheet.');
-    Logger.log(errorState);
     
     // Log to spreadsheet
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Errors");
@@ -107,59 +96,59 @@ function errorReporting(error,errorState,debug,currentEpochTime){
     sheet.getRange(sheet.getLastRow(), 1, 1, sheet.getLastColumn()).clearFormat();
     SpreadsheetApp.flush();
   } else {
-    if (currentTime.indexOf("AM")>-1) {
-      currentTime = currentTime.replace('AM','PM');
-    } else {
-      currentTime = currentTime.replace('PM','AM');
-    }
-  
+/*  
     // Log to spreadsheet    
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Success");
     sheet.appendRow(['Success', currentDate,currentTime,debug]);
     sheet.getRange(sheet.getLastRow(), 1, 1, sheet.getLastColumn()).clearFormat();
     SpreadsheetApp.flush();
+*/
   }
 }
 
-function getEpoch(input){
+function getEpoch(input,modifier){
+  if (modifier == null) {
+    modifier = 0;
+  }
   var rG = new RegExp('(.+)\\s([0-9]{2}),\\s(20[0-9]{2})\\sat\\s([0-9]{2})\\:([0-9]{2})(.+)');
   var p = input.match(rG);
   var a = twelve24(p[4]+':'+p[5]+' '+p[6]);
   var b = parseInt(getMonth(p[1]));
-  var io = (new Date(+p[3],+b,+p[2],+a,+p[5]).getTime() / 1000).toString();
-  return io;
+  var c = p[3]+'-'+(b+1)+'-'+p[2]+'T'+a+':'+p[5]+':00Z';
+  var io = (new Date(c).getTime()+modifier).toString();
+
+  return io.substring(0,9);
 }
 
 function getMonth(month){
    return new Date(Date.parse(month +" 1, 2012")).getMonth();
 }
 
-function epochToDate(Epoch) {
-//  var d = Date(Epoch * 1000);
-  var d = new Date(Epoch * 1000);
-  var time = new Array(2);
-      time[0] = Utilities.formatDate(d, 'America/Los_Angeles', "hh").toString();
-      time[1] = Utilities.formatDate(d, 'America/Los_Angeles', "mm").toString();
-      time[2] = Utilities.formatDate(d, 'America/Los_Angeles', "a").toString();
-  if (time[2] == 'AM'){
-    time[2]='PM';
-  }else{
-    time[2]='AM';
-  }
+function epochToDate(Epoch,modifier) {
+  if (modifier == null){
+    modifier = 0;
+  }  
+  var d = new Date(Number((Epoch*10000))+modifier);
+  var time = Utilities.formatDate(d, 'GMT', "hh-mm-a").toString().split('-');
   return time;
 }
 
 function twelve24(input) {
   var time = input;
-  var hours = Number(time.match(/^(\d+)/)[1]);
+  var hours = time.match(/^(\d+)/)[1].toString();
   var minutes = Number(time.match(/:(\d+)/)[1]);
-  var AMPM = time.match(/(.*)$/)[1].toLowerCase();
-  
-  if (AMPM == "pm" && hours < 12) hours = hours + 12;
-  if (AMPM == "am" && hours == 12) hours = hours - 12;
-  var sHours = hours.toString();
-  var sMinutes = minutes.toString();
-  if (hours < 10) sHours = "0" + sHours;
-  if (minutes < 10) sMinutes = "0" + sMinutes;
-  return sHours;
+  var part = time.match(/\s+(.*)$/)[1].toString().toUpperCase();
+  var toggle = false;
+
+  if (part == "PM") {
+    toggle = true;
+    hours = Number(hours)+12
+  } else {
+    toggle = 'Route1';
+    if (Number(hours) < 9 && hours.substring(0,1) !== "0") {
+      toggle = 'Route2';
+      hours = "0"+hours;
+    }
+  }
+  return hours;
 }
